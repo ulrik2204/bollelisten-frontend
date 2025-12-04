bfbb11b86227c9de738e6b476b14ca6dadaa52b1
Ulrik Røsby <ulrik.rosby@gjensidige.no>
2025-12-04 15:40:09 +0100

    fix: lint warning

--- src/utils/text-service.ts
+++ src/utils/text-service.ts
@@ -4,6 +4,7 @@ export function getText(key: keyof typeof fallbackTexts) {
   if (key in fallbackTexts) {
     return fallbackTexts[key];
   }
+  // eslint-disable-next-line no-console
   console.error(`Missing text for key: ${key}`);
   return "";
 }
